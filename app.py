f0ea8c31a3d08b7700d4af23d9a8d9538f2ecc13
emoodyv <colliermoody5@gmail.com>
2020-12-02 21:06:56 -0700

    Mission to Mars

--- app.py
+++ app.py
@@ -12,16 +12,16 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
+    return render_template('index.html', mars=mars)
 
 
 @app.route("/scrape")
 def scrape():
-   mars = mongo.db.mars
-   mars_data = scraping.scrape_all()
-   mars.update({}, mars_data, upsert=True)
-   return "Scraping Successful!"
+    mars = mongo.db.mars
+    mars_data = scraping.scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    return "Scraping Successful!"
 
 
 if __name__ == "__main__":
-   app.run()
+    app.run()
